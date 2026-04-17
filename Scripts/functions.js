43a2a531969fad64d62ee3d8ea53b4051d8ae279
Francesco Floris <francescofloris.ef@gmail.com>
2026-04-17 14:16:19 +0200

    aggiunta funzione per card, stile card e responsive

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,3 +1,21 @@
-function stampaCarte () {
 
+
+function stampaCarte(jsonData) {
+    let html = "";
+    for (let i = 0; i < jsonData.length; i++) {
+        const currentPic = jsonData[i];
+
+        html += `
+        <div class="col">
+            <div class="bg-white p-4 pin-item">
+                <img src="${currentPic.url}" class="card-img" alt="${currentPic.title}">
+                <div>
+                    <h2 class="title-card">${currentPic.title}</h2>
+                    <p class="date-card">${currentPic.date}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+    cardContainer.innerHTML = html;
 };
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,13 +1,12 @@
 const apiUrl = "https://lanciweb.github.io/demo/api/pictures/";
-
-
+let cardContainer = document.querySelector("#cards-container");
 
 fetch(apiUrl)
     .then(function (datiApi) {
         return datiApi.json();
     })
     .then(function (jsonData) {
-        // console.log(jsonData);
+        console.log(jsonData);
         stampaCarte(jsonData);
     })
     .catch(error => {
